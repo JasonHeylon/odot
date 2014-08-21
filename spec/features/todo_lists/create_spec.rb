4e72b085a92dd302f17fec3f55f2916a20451ea6
Jason Heylon <jasonheylon@gmail.com>
2014-08-22 00:10:12 +0800

    Add val to description

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -3,16 +3,24 @@ require "rails_helper"
 
 
 describe "Create todo lists" do
-	it "redirects to todo list index page on success" do
-  		visit "/todo_lists"
+
+	def create_todo_list(options={})
+		options[:title] ||= "My todo list"
+		options[:description] ||= "This is my todo list."
+
+		visit "/todo_lists"
 		click_link "New Todo list"
 		
 		expect(page).to have_content("New todo_list") 
 
-
-		fill_in "Title", with: "My todo list"
-		fill_in "Description", with: "This is what I'am doing today."
+		fill_in "Title", with: options[:title]
+		fill_in "Description", with: options[:description]
 		click_button "Create Todo list"
+	end
+
+
+	it "redirects to todo list index page on success" do
+  		create_todo_list
 
 		expect(page).to have_content("My todo list") 
 
@@ -21,15 +29,7 @@ describe "Create todo lists" do
 	it "display an error when the todo list has no title" do
 		expect(TodoList.count).to eq(0)
 
-		visit "/todo_lists"
-		click_link "New Todo list"
-		
-		expect(page).to have_content("New todo_list") 
-
-
-		fill_in "Title", with: ""
-		fill_in "Description", with: "This is what I'am doing today."
-		click_button "Create Todo list"
+		create_todo_list title: ""
 
 		expect(page).to have_content("error") 
 		expect(TodoList.count).to eq(0)
